64ca7fef142fd213146acc675335b91c03730498
Vince <babyaivan86@gmail.com>
2025-12-17 14:10:26 +0800

    Styled the transformation panel.

--- src/components/TransformationModes.tsx
+++ src/components/TransformationModes.tsx
@@ -3,18 +3,19 @@ import { useEditorStore } from "../store/editorStore"
 
 const TransformationModes = () => {
     const setTransformMode = useEditorStore(state => state.setTransformMode);
+    const transformMode = useEditorStore(state => state.transformMode);
 
   return (
     <div className="switch-panel">
-        <Move3D className="switch-panel-icon"
+        <Move3D className={`switch-panel-icon ${transformMode == "TRANSLATE" && 'active'}`}
             onClick={() => setTransformMode("TRANSLATE")}
         />
         <div className="switch-panel-horizontal-separator"></div>
-        <Rotate3D className="switch-panel-icon"
+        <Rotate3D className={`switch-panel-icon ${transformMode == "ROTATE" && 'active'}`}
             onClick={() => setTransformMode("ROTATE")}
         />
         <div className="switch-panel-horizontal-separator"></div>
-        <Scale3D className="switch-panel-icon"
+        <Scale3D className={`switch-panel-icon ${transformMode == "SCALE" && 'active'}`}
             onClick={() => setTransformMode("SCALE")}
         />
     </div>
