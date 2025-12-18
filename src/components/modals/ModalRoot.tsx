d18a765252b1b8af4e1e854a67f0e6a80fee140c
Vince <babyaivan86@gmail.com>
2025-12-18 13:07:34 +0800

    Handle outside clicks for closing modals.

--- src/components/modals/ModalRoot.tsx
+++ src/components/modals/ModalRoot.tsx
@@ -3,11 +3,18 @@ import Geometries from "./Geometries";
 
 const ModalRoot = () => {
     const activeModal = useEditorStore((s) => s.activeModal);
+    const closeModal = useEditorStore((s) => s.closeModal);
+
+    const handleClick = (e) => {
+        if (e.target.className === "modal-overlay")
+            closeModal();
+    };
 
     if (activeModal === "NONE") return null;
 
     return (
-        <div className="modal-overlay">
+        <div className="modal-overlay"
+            onClick={handleClick}>
             {activeModal === "GEOMETRY" && <Geometries/>}
         </div>
     )
