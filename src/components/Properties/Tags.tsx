eda24f033e9c599e8224c1feb84359415d058a63
Vince <babyaivan86@gmail.com>
2026-01-04 13:00:28 +0800

    Added wrappers for tags.

--- src/components/Properties/Tags.tsx
+++ src/components/Properties/Tags.tsx
@@ -1,5 +1,6 @@
 import { ChevronRight, X } from "lucide-react"
 import { useEditorStore } from "../../store/editorStore";
+import { useState } from "react";
 
 const Tags = () => {
     
@@ -10,22 +11,33 @@ const Tags = () => {
         t => t.target.kind === "OBJECT" && t.target.id === selectedObjectId
     );
 
+    const [paramsVisible, setParamsVisible] = useState(false);
+
   return (
     <div className="subpanel">
         <span className="subpanel-title">Tags</span>
 
         <div className="subpanel-container material">
             {objectTools.map(tool => (
-                <div className="tag" key={tool.id}>
-                    <div className="tag-wrapper">
-                    <ChevronRight
-                        className="tag-icon"
-                    />
-                    <span className="tag-name">
-                        {tool.type}
-                    </span>
+                <div className="tag-container">
+                    <div className="tag" key={tool.id}>
+                        <div className="tag-wrapper">
+                            <ChevronRight
+                                className="tag-icon"
+                            />
+                            <span className="tag-name">
+                                {tool.type}
+                            </span>
+                        </div>
+                        <X className="tag-icon"/>
                     </div>
-                    <X className="tag-icon"/>
+                    {paramsVisible && Object.entries(tool.parameters).map(([key, value]) => (
+                        <div className="params-container" key={key}> 
+                            <span className="property-name">
+                                {`${key}: `}
+                            </span>
+                        </div>
+                    ))}
                 </div>
             ))}
         </div>
