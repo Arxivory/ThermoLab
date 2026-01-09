a8386bf12eda84aa93f09916581ba2b12f8ed07e
Vince <babyaivan86@gmail.com>
2026-01-09 21:48:52 +0800

    Made the temperature application volume-based for now.

--- src/core/simulation/solvers/thermal/BoundaryHandlers.ts
+++ src/core/simulation/solvers/thermal/BoundaryHandlers.ts
@@ -19,10 +19,12 @@ export function applyBoundaryConditions(
 function applyFixedTemperature(grid: HeatGrid, T: number) {
     const { nx, ny, nz, temperature } = grid;
 
-    for (let i = 0; i < nx; i++) {
-        for (let j = 0; j < ny; j++) {
-            temperature[i + nx * j] = T;
-            temperature[i + nx * (j + ny * (nz - 1))] = T;
-        }
-    }
+    // for (let i = 0; i < nx; i++) {
+    //     for (let j = 0; j < ny; j++) {
+    //         temperature[i + nx * j] = T;
+    //         temperature[i + nx * (j + ny * (nz - 1))] = T;
+    //     }
+    // }
+
+    temperature.fill(T);
 }
