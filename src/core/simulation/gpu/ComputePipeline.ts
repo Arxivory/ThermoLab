effdf9f3b11bc0e8871d440b61dab5002139135c
Vince <babyaivan86@gmail.com>
2026-01-25 22:44:11 +0800

    Wrote the code base of the GPU compute pipeline.

---
+++ src/core/simulation/gpu/ComputePipeline.ts
@@ -0,0 +1,50 @@
+import type { HeatGrid } from "../solvers/thermal/HeatGrid";
+
+export class ComputePipeline {
+    private device: GPUDevice;
+    private pipeline: GPUComputePipeline;
+    private temperatureBuffer: GPUBuffer;
+    private volumeBuffer: GPUBuffer;
+
+    constructor(device: GPUDevice, shaderSource: string) {
+        this.device = device;
+
+        this.pipeline = device.createComputePipeline({
+            layout: 'auto',
+            compute: {
+                module: device.createShaderModule({ code: shaderSource }),
+                entryPoint: 'main'
+            }
+        });
+    }
+
+    setupBuffers(grid: HeatGrid) {
+        const size = grid.temperature.byteLength;
+
+        this.temperatureBuffer = this.device.createBuffer({
+            size,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST
+        });
+
+        this.volumeBuffer = this.device.createBuffer({
+            size,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        });
+
+        this.device.queue.writeBuffer(this.temperatureBuffer, 0, grid.temperature.buffer);
+        this.device.queue.writeBuffer(this.volumeBuffer, 0, grid.volumeFraction.buffer);
+    }
+
+    run(nx: number, ny: number, nz: number) {
+        const commandEncoder = this.device.createCommandEncoder();
+        const passEncoder = commandEncoder.beginComputePass();
+
+        passEncoder.setPipeline(this.pipeline);
+
+        passEncoder.dispatchWorkgroups(
+            Math.ceil(nx / 8),
+            Math.ceil(ny / 8),
+            Math.ceil(nz / 8),
+        )
+    }
+}
